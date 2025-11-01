efe26c917435f4b77376c80aa0bd9f6d39e905f1
Mithela Group <administrator@mithelagroup.com>
2025-11-01 16:21:21 +0600

    Delete app/models_contact.py

--- app/models_contact.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
-from app.database import Base
-
-class Contact(Base):
-    __tablename__ = "contacts"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(120), nullable=False)
-    mobile = Column(String(20), unique=True, index=True, nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
